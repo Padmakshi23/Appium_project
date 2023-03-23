bbc95ea52f6a1742c67add95821b1adf5edd5cb0
padmakshi <padmakshi231@gmail.com>
2023-03-23 11:20:46 +0530

    Completed task

--- Task2/test_task_2.py
+++ Task2/test_task_2.py
@@ -5,13 +5,15 @@ from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
 
 class AppiumConfig:
+
     @pytest.fixture(scope="function", autouse=True)
     def handle_app_launch(self):
         des_cap = {
 
             "platformName": "android",
             "deviceName": "oneplus",
-            "app": r"C:\Users\146698\Downloads\com.bsl.hyundai_2021-08-09.apk"
+            "app": r"C:\Users\146698\Downloads\com.bsl.hyundai_2021-08-09.apk",
+            #"udid":"9321e790"
         }
         self.driver = webdriver.Remote(command_executor="http://localhost:4723/wd/hub", desired_capabilities=des_cap)
         self.driver.implicitly_wait(30)
